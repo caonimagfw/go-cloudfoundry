6527aabeb34f57518cc54d487b91e24e372b491b
caonimagfw <51797166+caonimagfw@users.noreply.github.com>
2020-06-26 20:38:56 +0800

    22

--- c.sh
+++ c.sh
@@ -1,4 +1,3 @@
-chmod 777 /app/go/trojan-go-0.7.6
-chmod 777 /app/go/caddy
-nohup /app/go/caddy --conf="/app/go/caddy_config" -agree >> /app/go/caddy.log 2>&1 &
+chmod 777 /app/go/trojan-go-0.7.7
+nohup /app/go/caddy --conf="/app/go/caddy_config" 2>&1 &
 /app/go/trojan-go-0.7.6 --config /app/go/config.json
